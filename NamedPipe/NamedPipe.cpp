606a8e20e704685e0ea7cbd9c9327deb57cc6c49
joshua.griffith <joshua.griffith@bromium.com>
2014-07-09 14:27:50 +0100

    Changed buffer sizes and stuff

--- NamedPipe/NamedPipe.cpp
+++ NamedPipe/NamedPipe.cpp
@@ -12,11 +12,13 @@
 #define WRITING_STATE 2
 #define INSTANCES 4
 #define PIPE_TIMEOUT 5000
-#define BUFSIZE 4096
+#define BUFSIZE 512
+
+HANDLE hNamedPipe;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	HANDLE hNamedPipe = CreateNamedPipe(L"\\\\.\\pipe\\pipename", PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE, 1, 64, 64, PIPE_TIMEOUT, NULL);
+	hNamedPipe = CreateNamedPipe(L"\\\\.\\pipe\\pipename", PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE, 1, 64, 64, PIPE_TIMEOUT, NULL);
 	if (hNamedPipe == NULL || hNamedPipe == INVALID_HANDLE_VALUE)
 	{
 		std::cout << "Creation of named pipe failed" << std::endl;
@@ -29,15 +31,17 @@ int _tmain(int argc, _TCHAR* argv[])
 	{
 		std::cout << "Connection to named pipe failed" << std::endl;
 		std::system("PAUSE");
+		CleanUp();
 		return 1;
 	}
-	const wchar_t *data = L"HELLO WORLD";
+	const char *data = "HELLO WORLD\0";
 	DWORD bytesWritten = 0;
-	res = WriteFile(hNamedPipe, data, 64, &bytesWritten, NULL);
+	res = WriteFile(hNamedPipe, data, BUFSIZE, &bytesWritten, NULL);
 	if (!res)
 	{
 		std::cout << "Sending data to named pipe failed" << std::endl;
 		std::system("PAUSE");
+		CleanUp();
 		return 1;
 	}
 	else
@@ -45,7 +49,13 @@ int _tmain(int argc, _TCHAR* argv[])
 		std::cout << "Number of bytes written: " << bytesWritten << std::endl;
 		std::cout << "Sent data" << data << std::endl;
 		std::system("PAUSE");
+		CleanUp();
 		return 0;
 	}
+}
+
+void CleanUp()
+{
+	CloseHandle(hNamedPipe);
 }
 
--- NamedPipeClient/Program.cs
+++ NamedPipeClient/Program.cs
@@ -16,11 +16,11 @@ namespace NamedPipeClient
             Console.WriteLine("Object created");
             client.Connect();
             Console.WriteLine("Connected");
-            byte[] buff = new byte[64];
+            byte[] buff = new byte[512];
             Console.WriteLine("buffer prepared");
-            client.Read(buff, 0, 64); 
+            client.Read(buff, 0, 512); 
             Console.WriteLine("Data read");
-            Console.WriteLine(System.Text.Encoding.Default.GetString(buff));
+            Console.WriteLine(System.Text.Encoding.Default.GetString(buff).Split('\0')[0]);
             Console.WriteLine("Data printed, any key to exit...");
             Console.ReadLine();
         }
